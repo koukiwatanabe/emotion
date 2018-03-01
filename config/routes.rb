daeead717542f0a9e1598f15a17f50053738ba18
Kouki Watanabe <uguis.kouki@gmail.com>
2018-03-01 07:59:02 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # ログイン前トップページ
   root to: 'toppages#index'
   
+  # ログインページ
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
+  # ユーザー登録（サインアップ）
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
 
